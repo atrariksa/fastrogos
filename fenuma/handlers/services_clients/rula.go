401caf3b9663a3c1a798d4953d30a4c1ef8cffb1
atrariksa <53656746+atrariksa@users.noreply.github.com>
2021-12-21 14:29:22 +0700

    import and use rula client

---
+++ fenuma/handlers/services_clients/rula.go
@@ -0,0 +1,34 @@
+package services_clients
+
+import (
+	"github.com/atrariksa/fastrogos/fenuma/configs"
+	apiclient "github.com/atrariksa/fastrogos/rula/client"
+	"github.com/atrariksa/fastrogos/rula/client/operations"
+	rulamodels "github.com/atrariksa/fastrogos/rula/models"
+	httptransport "github.com/go-openapi/runtime/client"
+
+	"github.com/go-openapi/strfmt"
+)
+
+func GetRulaClient() *apiclient.Rula {
+	cfg := configs.GetFrom("../rula.env")
+
+	// create the transport
+	transport := httptransport.New(cfg.App.Hostname, "", nil)
+	transport.Debug = true
+
+	// create the API client, with the transport
+	client := apiclient.New(transport, strfmt.Default)
+
+	// to override the host for the default client
+	// apiclient.Default.SetTransport(transport)
+	return client
+}
+
+func SendCreateUserRequest(client *apiclient.Rula, req *rulamodels.ModelsCreateUserReq) (*operations.CreateUserCreated, error) {
+
+	createUserParam := operations.NewCreateUserParams()
+	createUserParam.ModelsCreateUserReq = req
+
+	return client.Operations.CreateUser(createUserParam)
+}
